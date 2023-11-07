88548419efbdb0de2b3e09036e6c5cc9b5faf30c
Yassine Hamil <y_hamil@hetic.eu>
2023-11-07 12:14:28 +0100

    db connect

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
+const mongoose = require('mongoose')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { DB_URI } = require('./config');
 
 var app = express();
+
+mongoose.connect(DB_URI)
+    .then((date) => console.log({date:'db connected'}))
+    .catch((err) => console.log(err))
 
 app.use(logger('dev'));
 app.use(express.json());
