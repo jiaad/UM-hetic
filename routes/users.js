6ca234ac4e078c0f96e7f9da7ca9152c8b1811c8
Yassine Hamil <y_hamil@hetic.eu>
2023-11-07 11:53:31 +0100

    add routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function(req, res, next) {
+  res.json({email: 'yasssine@mail.com' ,token: '15d4fsdf4ffsf'})
+});
+
+router.get('/create_account', function(req, res, next) {
+  res.json({email: 'yasssine@mail.com' ,token: '15d4fsdf4ffsf'})
+});
+
+router.get('/forget_password', function(req, res, next) {
+  res.json({email: 'yasssine@mail.com' ,token: '15d4fsdf4ffsf'})
+});
+
+router.get('/reset_password', function(req, res, next) {
+  res.json({email: 'yasssine@mail.com' ,token: '15d4fsdf4ffsf'})
+});
+
+router.get('/:id', function(req, res, next) {
+  res.json({email: 'yasssine@mail.com' ,token: '15d4fsdf4ffsf'})
+});
 module.exports = router;
