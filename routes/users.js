26760c6f746ed7f5214f9d32186eb43f6180b4d4
Yassine Hamil <y_hamil@hetic.eu>
2023-11-08 11:16:28 +0100

    create account

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,8 +11,15 @@ router.get('/login', function(req, res, next) {
   res.json({email: 'yasssine@mail.com' ,token: '15d4fsdf4ffsf'})
 });
 
-router.get('/create_account', function(req, res, next) {
-  res.json({email: 'yasssine@mail.com' ,token: '15d4fsdf4ffsf'})
+router.post('/create_account', async function(req, res, next) {
+  const {firstname, lastname , email, password, birth_date, gender, number, adresse } = req.body
+
+  if (email == undefined || password == undefined || firstname == undefined || lastname == undefined)
+  return res.json({ err:true , msg: `renseigner tout les champs`})
+  const doesExist = await User.findOne({email})
+  if(doesExist)
+  return res.json({success: true, msg: `${email} éxiste déja`})
+  const user = await User.create(req.body)
 });
 
 router.get('/forget_password', function(req, res, next) {
