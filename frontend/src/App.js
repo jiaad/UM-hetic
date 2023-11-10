41622077f7faf487b631411e7b97917dec351af3
Faithgg <fglindayi@gmail.com>
2023-11-10 10:20:56 +0100

    Reorganisation des fichiers,Installation de l'env frontend, Ajout de tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>front</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
