7d346aa6b888b9ff2d4391e07ff0118665d8e6b3
Faithgg <fglindayi@gmail.com>
2023-11-27 12:12:10 +0100

    Add express async handler

--- backend/routes/companies.js
+++ backend/routes/companies.js
@@ -1,14 +1,15 @@
 var express = require('express');
+const asyncHandler = require('express-async-handler');
 var router = express.Router();
 const companyCntrl = require('../controllers/company')
 const company_auth = require('../middleware/company.auth')
 /* GET Companies listing. */
-router.post('/', companyCntrl.create);
-router.post('/login', companyCntrl.login);
-router.get('/logout/:id', companyCntrl.logout);
-router.put('/:id', companyCntrl.update);
-router.delete('/:id', companyCntrl.delete);
-router.post('/forget_password', companyCntrl.forget_password);
-router.post('/reset_password/:token', companyCntrl.reset_password);
+router.post('/', asyncHandler(companyCntrl.create));
+router.post('/login', asyncHandler(companyCntrl.login));
+router.get('/logout/:id', asyncHandler(companyCntrl.logout));
+router.put('/:id', asyncHandler(companyCntrl.update));
+router.delete('/:id', asyncHandler(companyCntrl.delete));
+router.post('/forget_password', asyncHandler(companyCntrl.forget_password));
+router.post('/reset_password/:token', asyncHandler(companyCntrl.reset_password));
 
 module.exports = router;
