4cc1d946bf361714789aeba6d1b34ac41f6546b2
jiad-saegus <jiad.abdul@saegus.com>
2023-11-27 11:55:14 +0100

    react-router-form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Register from "./Components/company/register.jsx";
+import UserRegister from "./Components/company/register.jsx";
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>front</p>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<p />}>
+        <Route path="test" element={<p />} />
+        <Route path="tasks" element={<p />} />
+      </Route>
+      <Route path="/company" element={<Register />}>
+        <Route path="register" element={<Register />} />
+      </Route>
+      <Route path="/user" element={<p />}>
+        <Route path="register" element={<UserRegister />} />
+      </Route>
+      <Route path="about" element={<p />} />
+    </Routes>
   );
 }
 
---
+++ frontend/src/Components/user/register.jsx
@@ -0,0 +1,4 @@
+export default function UserRegister() {
+    return null;
+  }
+  
