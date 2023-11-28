785501184c365a727e90b23e8affb1093e25ae34
SirMacCready <polocharbel@gmail.com>
2023-11-28 09:34:39 +0100

    user

--- frontend/src/Components/user/register.jsx
+++ frontend/src/Components/user/register.jsx
@@ -1,4 +1,21 @@
+import React, {useState} from "react";
+
 export default function UserRegister() {
-    return null;
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("")
+  function onSubmit(event) {
+    event.preventDefault()
+
   }
-  
+  return (
+    <div className="container">
+      <form id="connectionForm" onSubmit={onSubmit} >
+                <label htmlFor="email">Company email </label>
+                <input type="text" id="email" name="Company email" placeholder="Company@email.com" required />
+                <label htmlFor="password">password </label>
+                <input type="text" id="password" name="password" placeholder="********" required />
+                <button type="submit">Login</button>
+      </form>
+    </div>
+    );
+}
