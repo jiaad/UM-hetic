24a69af715e9684d125d265dd1120418ccd5b9c8
jiad-saegus <jiad.abdul@saegus.com>
2023-11-28 12:23:53 +0100

    register

--- frontend/src/Components/company/login.jsx
+++ frontend/src/Components/company/login.jsx
@@ -1,91 +1,127 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export function Login() {
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
-	const [error, setError] = useState("")
-	const handleSubmit = async (e) => {
-		e.preventDefault()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [responseMsg, setResponseMsg] = useState("");
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [company, setCompany] = useState({});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		try {
-			const response = await fetch("http://localhost:3001/company/login", {
-				method: "POST",
-				headers: {
-					"Content-type": "application/json",
-				},
-				body: JSON.stringify({
-					email,
-					password,
-				}),
-			})
-			const res = await response.json()
-			if (res.ok) {
-				setError("Compagnie créée avec succès.")
-			} else {
-				setError(response.message)
-			}
-		} catch (error) {
-			console.error("Error submitting form:", error)
-			setError("Une erreur s'est produite lors de la soumission du formulaire.")
-		}
+    try {
+      const response = await fetch(
+        "http://fglindayi-um-hetic-code-redirect-3.apps.sandbox-m3.1530.p1.openshiftapps.com/company/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+      const res = await response.json();
+      if (res.success) {
+        console.log({ res, company: res.company });
+        setLoginSuccess(true);
+        setResponseMsg("Compagnie créée avec succès.");
+        setCompany(res.company);
+      } else {
+        setResponseMsg(response.message);
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      setResponseMsg(
+        "Une erreur s'est produite lors de la soumission du formulaire."
+      );
+    }
 
-		console.log("Login form submitted:", { email, password })
-	}
+    console.log("Login form submitted:", { email, password });
+  };
 
-	return (
-		<div className="min-w-md mx-auto mt-8 grid place-content-center h-[90vh]">
-			<div className="text-center flex flex-col gap-5 pb-5">
-				<h1 className="text-xl font-bold">Connexion</h1>
-			</div>
-			{error && <p className="text-red-500">{error}</p>}
-			<form onSubmit={handleSubmit} className="space-y-6">
-				<div>
-					<label
-						htmlFor="email"
-						className="block text-sm font-medium text-gray-700"
-					>
-						Email
-					</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-						className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-					/>
-				</div>
+  if (loginSuccess) {
+    return <Company company={company} />;
+  }
 
-				<div>
-					<label
-						htmlFor="password"
-						className="block text-sm font-medium text-gray-700"
-					>
-						Password
-					</label>
-					<input
-						type="password"
-						id="password"
-						name="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-						className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-					/>
-				</div>
+  return (
+    <div className="min-w-md mx-auto mt-8 grid place-content-center h-[90vh]">
+      <div className="text-center flex flex-col gap-5 pb-5">
+        <h1 className="text-xl font-bold">Connexion</h1>
+      </div>
+      {!loginSuccess && responseMsg && (
+        <p className="text-red-500">{responseMsg}</p>
+      )}
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+        </div>
 
-				<div>
-					<button
-						type="submit"
-						className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700"
-					>
-						Login
-					</button>
-				</div>
-			</form>
-		</div>
-	)
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+function Company({ company }) {
+  console.log(company);
+  return (
+    <section className="grid place-content-center h-[80vh] border rounded">
+      <article className="flex gap-4">
+        <p>Date de création:</p>
+        <p>{company?.date_of_creation}</p>
+      </article>
+      <article className="flex gap-4">
+        <p>Numéro de siret:</p>
+        <p>{company?.siret}</p>
+      </article>
+      <article className="flex gap-4">
+        <p>Type d'application:</p>
+        <p>{company?.type}</p>
+      </article>
+    </section>
+  );
+}
+
+export default Login;
--- frontend/src/Components/user/login.jsx
+++ frontend/src/Components/user/login.jsx
@@ -1,91 +1,124 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function Login() {
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
-	const [error, setError] = useState("")
-	const handleSubmit = async (e) => {
-		e.preventDefault()
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-		try {
-			const response = await fetch("http://localhost:3001/users/login", {
-				method: "POST",
-				headers: {
-					"Content-type": "application/json",
-				},
-				body: JSON.stringify({
-					email,
-					password,
-				}),
-			})
-			const res = await response.json()
-			if (res.ok) {
-				setError("Connecté !")
-			} else {
-				setError(response.message)
-			}
-		} catch (error) {
-			console.error("Error submitting form:", error)
-			setError("Une erreur s'est produite lors de la soumission du formulaire.")
-		}
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [user, setUser] = useState({});
 
-		console.log("Login form submitted:", { email, password })
-	}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-	return (
-		<div className="min-w-md mx-auto mt-8 grid place-content-center h-[90vh]">
-			<div className="text-center flex flex-col gap-5 pb-5">
-				<h1 className="text-xl font-bold">Connexion</h1>
-			</div>
-			{error && <p className="text-red-500">{error}</p>}
-			<form onSubmit={handleSubmit} className="space-y-6">
-				<div>
-					<label
-						htmlFor="email"
-						className="block text-sm font-medium text-gray-700"
-					>
-						Email
-					</label>
-					<input
-						type="email"
-						id="email"
-						name="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-						className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-					/>
-				</div>
+    try {
+      const response = await fetch(
+        "http://fglindayi-um-hetic-code-redirect-3.apps.sandbox-m3.1530.p1.openshiftapps.com/users/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+      const res = await response.json();
+      if (res.success) {
+        console.log({ res });
+        setLoginSuccess(true);
+        setError("Connecté !");
+        setUser(res.data);
+      } else {
+        setError(response.message);
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error.message, error);
+      setError(
+        "Une erreur s'est produite lors de la soumission du formulaire."
+      );
+    }
 
-				<div>
-					<label
-						htmlFor="password"
-						className="block text-sm font-medium text-gray-700"
-					>
-						Password
-					</label>
-					<input
-						type="password"
-						id="password"
-						name="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-						className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-					/>
-				</div>
+    console.log("Login form submitted:", { email, password });
+  };
 
-				<div>
-					<button
-						type="submit"
-						className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700"
-					>
-						Login
-					</button>
-				</div>
-			</form>
-		</div>
-	)
+  if (loginSuccess) {
+    return <User user={user} />;
+  }
+  return (
+    <div className="min-w-md mx-auto mt-8 grid place-content-center h-[90vh]">
+      <div className="text-center flex flex-col gap-5 pb-5">
+        <h1 className="text-xl font-bold">Connexion</h1>
+      </div>
+      {error && <p className="text-red-500">{error}</p>}
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+function User({ user }) {
+  console.log(user);
+  return (
+    <section className="grid place-content-center h-[80vh] border rounded">
+      <article className="flex gap-4">
+        <p>Nom:</p>
+        <p>{user?.firstname}</p>
+      </article>
+      <article className="flex gap-4">
+        <p>prénom:</p>
+        <p>{user?.lastname}</p>
+      </article>
+      <article className="flex gap-4">
+        <p>email:</p>
+        <p>{user?.email}</p>
+      </article>
+    </section>
+  );
+}
